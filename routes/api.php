947240a0197835cdb13aa0d998971e938f6f6f38
Luis <luis.aquino@edu.unifil.br>
2022-09-20 18:02:20 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+
+
+Route::post('Post/criarPostagem', [PostController::class,'criarPostagem']);
+
+Route::get('Post/index', [PostController::class, 'index']);
+Route::delete('Post/destroy/{id}', [PostController::class, 'destroy']);
+Route::put('Post/editPost/{id}', [PostController::class, 'editPost']);
+Route::get('Post/show/{id}', [PostController::class, 'show']);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+Route::post('Post/{id}/storeComment', [CommentController::class, 'storeComment']);
+Route::get('Post/{id}/indexComment', [CommentController::class, 'indexComment']);
+Route::delete('Post/{id}/destroyComment/{id_comments}', [CommentController::class, 'destroyComment']);
+Route::put('Post/{id}/editComment/{id_comments}', [CommentController::class, 'editComment']);
+Route::get('Post/{id}/showComment/{id_comments}', [CommentController::class, 'showComment']);
